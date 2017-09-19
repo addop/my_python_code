55ac1d958071c9bc5ca8245f2151ee438417c880
郑浩 <zhenghao@zhenghaodeMacBook-Pro-2.local>
2017-09-19 10:01:45 +0800

    甲方更改需求

--- start.py
+++ start.py
@@ -12,6 +12,6 @@ input_path = '/Users/zhenghao/Desktop/Chew_threshold_ana/idea/yuqingpeng/test'
 # ced视频+5, VR视频+3
 hang = 380
 lie = 367
-rectangle_scope = np.array([hang,(hang+5),lie,(lie+20)]) # 分析旧视频
+rectangle_scope = np.array([hang,(hang+5),lie,(lie+5)]) # 分析旧视频
 mf.pixel_record_2(input_path, rectangle_scope, video_form = 'mov', threshold_condition = 0.9, figure_condition_save = 'False')# 新视频
 #NB pixel_record_2(input_path, r_s, figure_condition_save = 'False', mode = 'real_time', bolt = 25, threshold_condition = 0.95)
