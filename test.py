b675f90d65fb900249cdca7e7cd747f16107d9b6
郑浩 <zhenghao@zhenghaodeMacBook-Pro-2.local>
2017-09-19 13:16:21 +0800

    update

--- test.py
+++ test.py
@@ -1,10 +1,4 @@
-import matplotlib.pyplot as plt
-from tqdm import tqdm
-
-a = 
-
-
-plt.figure(figsize=(500,50))
-plt.plot(list_result, 'k')
-plt.savefig(filename, dpi = 100, bbox_inches = 'tight')
-print('图像保存完毕')
+a = 1
+b = 2
+c = a+1
+print(c)
