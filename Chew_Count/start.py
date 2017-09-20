0a3da1f504a5c72be43e2b96f6e76c122d3a8f6f
郑浩 <zhenghao@zhenghaodeMacBook-Pro-2.local>
2017-09-20 14:37:12 +0800

    新建Chew代码文件夹

---
+++ Chew_Count/start.py
@@ -0,0 +1,30 @@
+import numpy as np
+import def_baggage_666 as db6
+import ana_openCV as mf
+
+input_path = '/Users/zhenghao/Desktop/Chew_threshold_ana/idea/yuqingpeng_2/video/20170905/1-1'
+
+# 输入要分析的像素范围
+# 软件截图后, 在'预览'中打开并圈出对应区域的两个顶点
+# 例如:
+# 284 442 ->268 441
+# 新视频: 左上角: 450->384
+# ced视频+5, VR视频+3
+
+# 视频参数调节
+# 输入要分析的区域和模式
+有多少行 = 383
+有几个列 = 654
+多大的正方形 = 5
+rectangle_scope = np.array([有多少行,(有多少行+多大的正方形),有几个列,(有几个列+多大的正方形)]) # 分析旧视频
+# 输入LED点亮的区域
+LED点亮行 = 0
+LED点亮列 = 0
+LED_scope = np.array([LED点亮行,(LED点亮行+2),LED点亮列,(LED点亮列+2)])
+# 选取分析模式
+试验台是CED还是VR = 'CED'
+
+# 进行分析
+mf.pixel_record_2(input_path, rectangle_scope, LED_scope, video_form = 'mpg', threshold_condition = 0.9, figure_condition_save = 'False', video_mode = 试验台是CED还是VR, skip_frame = 2125)# 新视频
+print ('\a'*7) # 程序完结后发出声音
+#NB pixel_record_2(input_path, r_s, figure_condition_save = 'False', mode = 'real_time', bolt = 25, threshold_condition = 0.95, video_mode = 'CED')
