c8c71fd2a123e5cdfb9c0cb867039dcb915dacee
HaoZ <1733473428@qq.com>
2019-04-04 13:53:20 +0800

    网上copy了一段代码, 感觉这个函数很棒, 能够直接拿来用, 非常赞

--- 股票投资/main.py
+++ 股票投资/main.py
@@ -1,5 +1,77 @@
 # - [ ] 通过爬虫获得基金数据
 
-import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import re
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
 
+#指定默认字体
+matplotlib.rcParams['font.sans-serif'] = ['SimHei']
+matplotlib.rcParams['font.family']='sans-serif'
+#解决负号'-'显示为方块的问题
+matplotlib.rcParams['axes.unicode_minus'] = False
+
+# 抓取网页
+def get_url(url, params=None, proxies=None):
+    rsp = requests.get(url, params=params, proxies=proxies)
+    rsp.raise_for_status()
+    return rsp.text
+
+# 从网页抓取数据
+def get_fund_data(code,per=10,sdate='',edate='',proxies=None):
+    url = 'http://fund.eastmoney.com/f10/F10DataApi.aspx'
+    params = {'type': 'lsjz', 'code': code, 'page':1,'per': per, 'sdate': sdate, 'edate': edate}
+    html = get_url(url, params, proxies)
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # 获取总页数
+    pattern=re.compile(r'pages:(.*),')
+    result=re.search(pattern,html).group(1)
+    pages=int(result)
+
+    # 获取表头
+    heads = []
+    for head in soup.findAll("th"):
+        heads.append(head.contents[0])
+
+    # 数据存取列表
+    records = []
+
+    # 从第1页开始抓取所有页面数据
+    page=1
+    while page<=pages:
+        params = {'type': 'lsjz', 'code': code, 'page':page,'per': per, 'sdate': sdate, 'edate': edate}
+        html = get_url(url, params, proxies)
+        soup = BeautifulSoup(html, 'html.parser')
+
+        # 获取数据
+        for row in soup.findAll("tbody")[0].findAll("tr"):
+            row_records = []
+            for record in row.findAll('td'):
+                val = record.contents
+
+                # 处理空值
+                if val == []:
+                    row_records.append(np.nan)
+                else:
+                    row_records.append(val[0])
+
+            # 记录数据
+            records.append(row_records)
+
+        # 下一页
+        page=page+1
+
+    # 数据整理到dataframe
+    np_records = np.array(records)
+    data= pd.DataFrame()
+    for col,col_name in enumerate(heads):
+        data[col_name] = np_records[:,col]
+
+    return data
+
+data=get_fund_data('161725',per=49,sdate='2019-01-01',edate='2019-4-1')
+print(data)
